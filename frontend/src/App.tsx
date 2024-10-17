967a0f8ff464df607ae38ff72563324fc4e8eab4
kshetritej <tejghartikshetri@gmail.com>
2024-10-17 20:40:59 +0545

    add: vite proxy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export const App = () => {
   const [totalSpent, setTotalSpent] = useState(0);
+
+  useEffect(() => {
+    fetch("/api/expenses/total-spent")
+      .then((res) => res.json())
+      .then((data) => {
+        setTotalSpent(data.totalSpent);
+      });
+  }, [])
   return <div>
     <Card>
       <CardHeader>
